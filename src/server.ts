4081e9bca157f2a84589b62253c9eaa305f6ebd4
Jahid Hasan (Morshed) <jh3495689@gmail.com>
2026-02-22 00:46:02 +0600

    updated all code

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from './app.js';
+import config from './app/config/index.js';
+
+import mongoose from 'mongoose';
+
+async function main() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
