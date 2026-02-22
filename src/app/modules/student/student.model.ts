c2feefd72955b1aff71228e8b17c02033da26814
Jahid Hasan (Morshed) <jh3495689@gmail.com>
2026-02-22 17:47:28 +0600

    model builtin validation full updated

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -2,56 +2,68 @@ import { Schema, model } from 'mongoose';
 import type { Guardian, LocalGuardian, Student, Username } from './student.interface.js';
 
 const userNameSchema = new Schema<Username>({
-  firstName: { type: String, required: true },
+  firstName: { type: String, required: [true, 'First name must be required'] },
   middleName: { type: String },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: [true, 'Last name must be required'] },
 });
 
 const guardianSchema = new Schema<Guardian>({
-  fatherName: { type: String, required: true },
-  fatherOccupation: { type: String, required: true },
-  fatherContactNo: { type: String, required: true },
-  motherName: { type: String, required: true },
-  motherOccupation: { type: String, required: true },
-  motherContactNo: { type: String, required: true },
+  fatherName: { type: String, required: [true, 'Father Name must be required'] },
+  fatherOccupation: { type: String, required: [true, 'Father occupation must be required'] },
+  fatherContactNo: { type: String, required: [true, 'Father contact no must be required'] },
+  motherName: { type: String, required: [true, 'Mother name must be required'] },
+  motherOccupation: { type: String, required: [true, 'Mother occupation must be required'] },
+  motherContactNo: { type: String, required: [true, 'Mother contact no must be requried'] },
 });
 
 const LocalGuardianSchema = new Schema<LocalGuardian>({
-  name: { type: String, required: true },
-  occupation: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  address: { type: String, required: true },
+  name: { type: String, required: [true, 'Local Guardian Name must be required'] },
+  occupation: { type: String, required: [true, 'Local Guardian occupation must be required'] },
+  contactNo: { type: String, required: [true, 'Local Guardian contact no must be required'] },
+  address: { type: String, required: [true, 'Local Guardian address must be required'] },
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String },
+  id: { type: String, unique: true, required: true },
   name: {
     type: userNameSchema,
+    required: [true, 'Name must be required'],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female'],
+      message: '{VALUE} is not supported',
+    },
     required: true,
   },
-  gender: { type: String, enum: ['male', 'female'], required: true },
-  dateOfBirth: { type: String, required: true },
-  email: { type: String, required: true },
-  contactNo: { type: String, required: true },
-  emergencyContactNo: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contactNo: { type: String, required: [true, 'Contact no must be required'] },
+  emergencyContactNo: { type: String, required: [true, 'Emergency contact no must be required'] },
   bloodGroup: {
     type: String,
-    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+      message: '{VALUE} is not supported',
+    },
   },
-  presentAddress: { type: String, required: true },
-  permanentAddress: { type: String, required: true },
+  presentAddress: { type: String, required: [true, 'Present Address must be required'] },
+  permanentAddress: { type: String, required: [true, 'Permanent Address must be required'] },
   guardian: {
     type: guardianSchema,
-    required: true,
+    required: [true, 'Guardian information must be required'],
   },
   localGuardian: {
     type: LocalGuardianSchema,
-    required: true,
+    required: [true, 'Local Guardian information must be required'],
   },
   profileImg: { type: String },
   isActive: {
     type: String,
-    enum: ['active', 'inactive'],
+    enum: {
+      values: ['active', 'inactive'],
+      message: '{VALUE} is not supported',
+    },
     default: 'active',
     required: true,
   },
