3a3ee1daa02325ff2afd0c07ec08b7b85fc2ae4c
Jahid Hasan (Morshed) <jh3495689@gmail.com>
2026-03-05 19:41:52 +0600

    user interface user model or user validation add

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose';
+import type { TUser } from './user.interface.js';
+
+const userSchema = new Schema<TUser>(
+  {
+    id: { type: String, required: true },
+    password: { type: String, required: true },
+    needsPasswordChange: { type: Boolean, default: true },
+    role: { type: String, enum: ['admin', 'student', 'faculty'] },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
